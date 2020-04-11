3ea48884b1062b6a99795afdd310ffcac19efeff
Guosa Edebiri <gedebiri@gmail.com>
2020-04-11 23:23:52 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -12,6 +12,7 @@ def prompt_user
   puts "2.) Eat a ham."
 end
 
+binding.pry
 def selection(num)
   if num = 1
      "YUM YUM MUNCH MUNCH MUNCH"
@@ -19,7 +20,6 @@ def selection(num)
      "HAM HAM HAM IN MY TUMMY"
   end
 end
-binding.pry 
 
 def runner
   prompt_user
