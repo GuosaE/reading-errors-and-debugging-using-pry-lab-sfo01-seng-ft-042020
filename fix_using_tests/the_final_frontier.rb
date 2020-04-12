59bb3a098d34f7413c7b30c56ca7989d3200d066
Guosa Edebiri <gedebiri@gmail.com>
2020-04-12 01:51:40 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -3,7 +3,7 @@
 require 'pry'
 
 def generate_star_date
-  generate_star_date = (rand(100000) + 400000) / 10.0
+  (rand(100000) + 400000) / 10.0
 end
 
 
@@ -12,6 +12,6 @@ def state_log(star_date)
 end
 
 def engage
-  puts state_log(date)
   date = generate_star_date
+  puts state_log(date)
 end
